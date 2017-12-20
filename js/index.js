71f070b962ed0619ea1563eca1e60dcb2e92c548
Charlotte Joy Callender <charlottejoyliving@gmail.com>
2017-12-20 07:51:01 -0500

    Made toggle function and trigger more flexible/ less redundant. Increased to 25 squares. Added winner check + alert. Added list of important to-adds and some nice-to-haves.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,64 @@
+var size= 5;
+
+function toggleAround(clicked){
+  
+var numClick= Number(clicked);// if this is not converted to a number, we get bad math (5+3=53)
+  
+var center = document.getElementById(numClick);
+    center.classList.toggle("on");
+
+
+if(numClick%size!=0 && numClick+1<=size*size){//if not at end of row
+ var right = document.getElementById(numClick+1);
+    right.classList.toggle("on");
+}
+
+  if(numClick%size!=1  && numClick-1>0){//if not at beginning of row
+ var left = document.getElementById(numClick-1);
+    left.classList.toggle("on");
+  }
+  
+  if(numClick+size<=size*size){//if not last row
+ var below = document.getElementById(numClick+size);
+    below.classList.toggle("on");
+  }
+ 
+ if(numClick-size>0){ //if not first row
+ var above = document.getElementById(numClick-size);
+    above.classList.toggle("on");
+ } 
+  
+   var allOff= $("div").hasClass( "on" );
+  if(allOff==false){
+    alert("winner");
+    //option to play again vs next game
+  }
+}
+
+
+$(".square").click(function(){
+ var clickedSquare=  $(this).attr('id'); //onClick grab id and run through toffle function
+  toggleAround(clickedSquare);
+                   });
+
+
+/*important
+
+winable game levels added
+function game1(){
+  //create 1st pattern which will be set before any JS runs
+}
+
+*/
+/*NICE TO HAVES
+- customizable size/ easy, medium, hard sizes || how this will work with winable games?
+- count moves against least number of moves required->points
+- 
+*/
+
+
+
+/* A less flexible method
 var s1= document.getElementById("1");
 var s2= document.getElementById("2");
 var s3= document.getElementById("3");
@@ -8,32 +69,6 @@ var s7= document.getElementById("7");
 var s8= document.getElementById("8");
 var s9= document.getElementById("9");
 
-var size= 3;
-
-function toggleAround(clicked){
-  
-   var element = document.getElementById(clicked);
-    element.classList.toggle("on");
-  
- var id1= clicked+1
- var element = document.getElementById(id1);
-    element.classList.toggle("on");
-  
-   var id2= clicked-1
- var element = document.getElementById(id2);
-    element.classList.toggle("on");
-  
-   var id3= clicked+size
- var element = document.getElementById(id3);
-    element.classList.toggle("on");
-  
-   var id4= clicked-size
- var element = document.getElementById(id4);
-    element.classList.toggle("on");
-  alert(clicked);
-}
-
-s1.onclick= ()=>toggleAround(1);
 s2.onclick= ()=>toggleAround(2);
 s3.onclick= ()=>toggleAround(3);
 s4.onclick= ()=>toggleAround(4);
@@ -41,4 +76,4 @@ s5.onclick= ()=>toggleAround(5);
 s6.onclick= ()=>toggleAround(6);
 s7.onclick= ()=>toggleAround(7);
 s8.onclick= ()=>toggleAround(8);
-s9.onclick= ()=>toggleAround(9);
+s9.onclick= ()=>toggleAround(9);*/
